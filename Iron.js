369289f9b75a85e4b51495277b7beebd1ba403ef
jonymelch15 <87871678+jonymelch15@users.noreply.github.com>
2021-09-09 11:02:23 -0500

    Add files via upload
    primera vez que se sube

---
+++ Iron.js
@@ -0,0 +1,30 @@
+class Iron{
+	constructor(x,y)
+	{
+	    var options = {
+			'density':30,
+			'friction': 3,
+			'restitution':0.8
+		  };
+		this.body = Bodies.rectangle(x, y, 50, 50, options);
+		this.width = 50;
+		this.height = 50;
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var pos=this.body.position;		
+			push()
+			translate(pos.x, pos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("white");
+			fill("white");
+            rect(0, 0, this.width, this.height);
+		
+
+			pop()
+	}
+
+}
